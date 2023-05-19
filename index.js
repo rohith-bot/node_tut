b65061caab24f1842118088e1120a557dc1126e8
rohith_sid <rohithssiddeshwara@gmail.com>
2023-05-19 20:27:23 +0530

    path module

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const math = require("./math.js")
+const path = require("node:path")
 
-const {add , subtract} = math
+console.log(__filename);
+console.log(__dirname);
 
-console.log(add(2,3));
-console.log(subtract(5,4));
+console.log(path.basename(__filename))
+console.log(path.basename(__dirname))
+
+console.log(path.extname(__filename))
+console.log(path.extname(__dirname))
+
+console.log(path.parse(__filename))
+let obj1 = path.parse(__filename);
+
+console.log(path.format(obj1))
+
+console.log(path.join("folder1","folder2","index.js"))
+console.log(path.resolve("folder1","folder2","index.js"))
+console.log(path.resolve("/folder1","folder2","index.js"))
+console.log(path.resolve("/folder1","//folder2","index.js"))
+console.log(path.resolve("/folder1","//folder2","../index.js"))
