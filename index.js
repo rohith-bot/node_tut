d3d6d5c2bda2a8153f1ba47697b7b25c1f233ca1
rohith_sid <rohithssiddeshwara@gmail.com>
2023-05-19 15:34:34 +0530

    ESmodule

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { superman as sup } from "./superman.js";
-import { batman } from "./batman.js";
+const math = require("./math.js")
 
-sup();
-batman();
+const {add , subtract} = math
+
+console.log(add(2,3));
+console.log(subtract(5,4));
