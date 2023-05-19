bc54f85a55d2300f6e9f087eec48484e9abd422f
rohith_sid <rohithssiddeshwara@gmail.com>
2023-05-20 00:14:10 +0530

    fs module

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
-const buffer = new Buffer.from("Rohith")
+const fs = require("node:fs/promises")
 
-console.log(buffer)
-console.log(buffer.toJSON())
-console.log(buffer.toString())
+async function read(){
+    const data = await fs.readFile("greet.txt")
+    console.log(data)
+}
+
+
+read()
+
+
+// fs.readFile("greet.txt","utf-8")
+// .then(data =>{
+//     console.log(data)
+// }).catch(err => {
+//     console.log(err)
+// })
+
+
+
+
+
+
+
+
+
+
+// // // const data = fs.readFileSync("./textFiletxt")//this is synchronous
+
+// // // console.log(data.toString())
+
+// // fs.readFile("./textFiletxt",(err,data1) =>{
+// //     console.log(data1)
+// // })
+
+// fs.writeFileSync("./greet.txt","hello\nhi");
+
+// fs.writeFile("./greet.txt","Heloo I am Async",{flag:"a"} ,(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("data written")
+//     }
+// })
