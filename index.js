aace024bf9482c1e91bfe3ab4dead8e6d11785b6
rohith_sid <rohithssiddeshwara@gmail.com>
2023-05-19 23:21:43 +0530

    buffer

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
-const PizzaShop = require("./pizza-shop.js")
-const DrinkMachine = require("./drinkMachine.js")
+const buffer = new Buffer.from("Rohith")
 
-const dominos = new PizzaShop()
-const drinks = new DrinkMachine()
-
-dominos.on("pizza-order",(size,toppings)=>{
-    console.log(`${size} Pizza is baking with ${toppings}`)
-    drinks.serveDrink(size)
-})
-dominos.order("large", "olives");
-dominos.displayOrderno();
-
-
-
-
-// const EventEmitter = require("node:events")
-
-// const emitter = new EventEmitter()
-
-// emitter.on("order-pizza",(size,drinks)=>{
-//     console.log(`Order recieved! baking ${size} size pizza with ${drinks}`)
-// })
-
-// emitter.on("order-pizza", (size)=>{
-//     if(size === 'large'){
-//         console.log(`you are a member now`)
-//     }
-// })
-
-// emitter.emit("order-pizza", "regular", "coke")
+console.log(buffer)
+console.log(buffer.toJSON())
+console.log(buffer.toString())
