615e65c6d17230f16a8e158117b552ddb67c717f
rohith_sid <rohithssiddeshwara@gmail.com>
2023-05-21 09:55:02 +0530

    http index

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require("node:http")
+const fs = require("node:fs")
 
 const server = http.createServer((req,res)=>{
     
     res.writeHead(200,{"Content-Type":"text/html"})
-    res.end("<h1>Hello World</h1>");
+    fs.createReadStream(__dirname+"/index.html").pipe(res)
 })
 
 server.listen(5000,()=>{
