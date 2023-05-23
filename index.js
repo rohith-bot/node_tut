ee47af723ac2b6cee2976454c32efec2cefd04c7
rohith_sid <rohithssiddeshwara@gmail.com>
2023-05-23 22:36:59 +0530

    http

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const http = require("node:http")
 const fs = require("node:fs")
 
-const server = http.createServer((req,res)=>{
-   res.writeHead(200)
-   
-   fs.writeFile("request.json",json,()=>{console.log("writting to file")})
-   res.end("writting")
-})
+
+const server = http.createServer() 
+
 
 server.listen(5000,()=>{
     console.log("Server running on port 5000")
