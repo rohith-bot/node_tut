ee912801aa78f998d49fc33f1aea9846e7a3063b
rohith_sid <rohithssiddeshwara@gmail.com>
2023-05-24 21:40:16 +0530

    libuv

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const http = require("node:http")
-const fs = require("node:fs")
+const fs = require("node:fs");
 
 
-const server = http.createServer() 
+process.env.THREADPOOL_SIZE = 1;
+
+const calls = 10;
+const start = Date.now()
+for(let i = 0; i<calls; i++){
+    fs.readFile("file.txt","utf-8",(err,data)=>{
+        console.log("read")
+    });
+    console.log(Date.now() - start)
+}
 
 
-server.listen(5000,()=>{
-    console.log("Server running on port 5000")
-})
+
+
+
+
